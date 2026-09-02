e532e7aaee22ebc2aebe6e371c592365383ad18c
ron <abilfattoeyo@gmail.com>
2026-09-02 12:09:15 +0900

    feat: FrontEnd JSni qurish

--- app.js
+++ app.js
@@ -33,12 +33,8 @@ app.post("/create-item", (req, res) => {
     console.log(req.body);   
     const new_reja = req.body.reja;
     db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
-        if(err) {
-            console.log(err)
-            res.end("Something went wrong!")
-        } else {
-            res.end("Successfully added");
-        }
+       console.log(data.ops);
+       res.json(data.ops[0]);
     })
     // res.json({ test: "success"})     
 })
