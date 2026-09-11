8ca5e54afb428cf41d3a0828f3faef55b75992d2
ron <abilfattoeyo@gmail.com>
2026-09-11 19:41:01 +0900

    fix: PORT changed

--- server.js
+++ server.js
@@ -2,27 +2,30 @@ const http = require("http");
 const mongodb = require("mongodb");
 
 let db;
-const connectionString = 
-  "mongodb+srv://abilfattoevozodbek:h45JMqnXSHrGoxu5@cluster0.nuorz68.mongodb.net/Reja"
+const connectionString =
+  "mongodb+srv://abilfattoevozodbek:h45JMqnXSHrGoxu5@cluster0.nuorz68.mongodb.net/Reja";
 
-mongodb.connect(connectionString, {     // DATABASE va BACKENDni o'zaro ulash
+mongodb.connect(
+  connectionString,
+  {
+    // DATABASE va BACKENDni o'zaro ulash
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}, (err, client) => {
-   if(err) console.log("ERROR on connection MongoDB");
-else {
-    console.log("MongoDB connection succeed");
-    module.exports = client.db();;
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDB connection succeed");
+      module.exports = client.db();
 
-    const app = require("./app");
-    const server = http.createServer(app);
-    let PORT = 3000;
-    server.listen(PORT, function() {
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 8014;
+      server.listen(PORT, function () {
         console.log(
-            `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+          `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`,
         );
-    });
-}
-});
-
-
+      });
+    }
+  },
+);
